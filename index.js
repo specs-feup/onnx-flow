0ce7e12aa581aabb441baa6c5243f83edb17b0ca
nelsonneto21 <nelsonjoneto@gmail.com>
2024-10-02 15:32:15 +0100

    Divided the transformation of operations into two steps, one that transforms and other that optimizes the operations

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import {createGraph } from './initGraph.js'
-import {transformOpps} from './transformOpps.js'
+import {transformOpps, optimizeForDimensions} from './transformOpps.js'
 import {generateCode} from './codeGeneration.js'
 import {layoutAndStyling } from './layoutAndStyling.js'
 
 Promise.all([
-  fetch('AddAddAdd.json')
+  fetch('MatMul.json')
     .then(function(res) {
       return res.json();
     }),
@@ -15,16 +15,18 @@ Promise.all([
 ])
 .then(function(dataArray) {
     let cy = createGraph(dataArray[0])
-    //transformOpps(cy)
+    transformOpps(cy)
+    optimizeForDimensions(cy)
     //generateCode(cy, dataArray[0])
     layoutAndStyling(cy,dataArray[1])
 });
 
+//select the operation nodes with no parent and check if there are optimizations to be made
 
 //FAZER SLIDES
 //colocar otimizações de lado por enquanto
 //adicionar suporte para n dimensões e utilizar a ferramenta onnx2json
 //gerar os testes -> testes de integração são
-//tornar a coisa funcional
+//tornar a coisa funcional, ou seja, dividir em passos
 
 // MAIS TARDE LER LITERATURA SOBRE OTIMIZAÇÃO DE OPERAÇÕES
