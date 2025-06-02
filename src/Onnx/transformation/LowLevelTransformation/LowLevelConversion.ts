7e7ffb02ea2788d7d65490ea000ee38e2227ed98
Luís Alves <luiscnalves@gmail.com>
2025-06-02 18:59:52 +0100

    Add add chain visualization

--- src/Onnx/transformation/LowLevelTransformation/LowLevelConversion.ts
+++ src/Onnx/transformation/LowLevelTransformation/LowLevelConversion.ts
@@ -7,22 +7,29 @@ import OnnxGraph from "../../OnnxGraph.js"
 import transformSimpleLoopOperations from "./TransformSimpleLoopOperations.js";
 import transformMatMul from "./TransformMatMul.js";
 import transformLoop from "./TransformLoop.js";
+import transformChain from "./TransformChain.js";
 
 
 export default class OnnxGraphTransformer
     implements Graph.Transformation<OnnxGraph.Class, OnnxGraph.Class>
 {
     apply(graph: OnnxGraph.Class): OnnxGraph.Class {
+        const operationCount = graph.nodes.filterIs(OperationNode).length;
 
-        graph.nodes.filterIs(OperationNode).forEach(node => {
-            if (node.type == "MatMul") {
-                transformMatMul(node, graph)
-            } else if (node.type == "Loop") {
-                transformLoop(node, graph)
-            } else {
-                transformSimpleLoopOperations(node, graph)
-            }
-        })
+        if (operationCount === 3) {
+            const addNodes = graph.nodes.filterIs(OperationNode).filter(node => node.type === "Add").toArray();
+            transformChain(addNodes, graph);
+        } else {
+            graph.nodes.filterIs(OperationNode).forEach(node => {
+                if (node.type == "MatMul") {
+                    transformMatMul(node, graph)
+                } else if (node.type == "Loop") {
+                    transformLoop(node, graph)
+                } else {
+                    transformSimpleLoopOperations(node, graph)
+                }
+            })
+        }
 
         // Return the modified graph
         return graph;
