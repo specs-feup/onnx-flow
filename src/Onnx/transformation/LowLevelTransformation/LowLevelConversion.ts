12cb88d8fda4bd831d1d807192e6340efe3a5ec0
José Pedro Ferreira <zepepf@gmail.com>
2025-09-23 23:36:17 +0100

    Add loop optimizations as flags

--- src/Onnx/transformation/LowLevelTransformation/LowLevelConversion.ts
+++ src/Onnx/transformation/LowLevelTransformation/LowLevelConversion.ts
@@ -5,7 +5,13 @@ import TransformChain from "./TransformChain.js";
 export default class OnnxGraphTransformer
   implements Graph.Transformation<OnnxGraph.Class,OnnxGraph.Class>
 {
+  constructor(
+    private fuse: boolean = true,
+    private recurse: boolean = false,
+    private coalesce: boolean = true
+  ) {}
+
   apply(graph: OnnxGraph.Class): OnnxGraph.Class {
-    return new TransformChain(true, false, true).apply(graph);
+    return new TransformChain(this.fuse, this.recurse, this.coalesce).apply(graph);
   }
 }
