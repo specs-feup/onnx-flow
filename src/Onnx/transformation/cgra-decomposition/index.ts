47d6508323d1cce331f513f32145c5276e493999
Process-ing <rbruno.p.o@gmail.com>
2025-12-29 15:31:53 +0000

    Improve decomposition registration

--- src/Onnx/transformation/cgra-decomposition/index.ts
+++ src/Onnx/transformation/cgra-decomposition/index.ts
@@ -1,15 +1,20 @@
 import OnnxGraph from "../../OnnxGraph.js";
 import { decomposeMatMul } from "./decomposers/MatMul.js";
 
+const decomposers = {
+  MatMul: decomposeMatMul,
+};
+
 export default function transformForCgra(g: OnnxGraph.Class): OnnxGraph.Class {
   let anyDivided = true;
 
   while (anyDivided) {
     anyDivided = false;
-    const operationNodes = g.getOperationNodes().toArray();
+    const operationNodes = g.getOperationNodes();
 
     for (const node of operationNodes) {
-      if (node.type === "MatMul" && decomposeMatMul(node, g)) {
+      const decomposer = decomposers[node.type];
+      if (decomposer !== undefined && decomposer(node, g)) {
         anyDivided = true;
       }
     }
